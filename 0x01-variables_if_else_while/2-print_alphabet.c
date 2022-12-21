61ed77c5de56491b06ab98bc16c9ef56fa782aed
Daud1000 <abdulazeezdaud726@gmail.com>
2022-12-21 13:56:09 -0800

    return (0);

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,18 +7,13 @@
  */
 int main(void)
 {
-	char ch;
-
+        char ch;
 	for (ch = 'a'; ch <= 'z'; ch++)
-
-	
-	{
-
-		putchar(ch);
-	}
-	
-	putchar('\n');
-	return (0);
-
+	    {
+		    putchar(ch);
+            }
+	    
+	    putchar('\n');
+            return (0);
 }
 
