47bf30e37042328e343da951fa40132e578a3f11
Daud1000 <abdulazeezdaud726@gmail.com>
2022-12-23 07:27:43 -0800

    4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -12,8 +12,8 @@
 
 void print_line(char *c, int s, int l)
 {
-
 	int j, k;
+
 	for (j = 0; j <= 9; j++)
 	{
 		if (j <= s)
@@ -28,7 +28,7 @@ void print_line(char *c, int s, int l)
 		if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
 			putchar(c[l * 10 + k]);
 		else
-putchar('.');
+			putchar('.');
 	}
 }
 
@@ -41,7 +41,6 @@ putchar('.');
  */
 void print_buffer(char *b, int size)
 {
-
 	int i;
 
 	for (i = 0; i <= (size - 1) / 10 && size; i++)
@@ -49,13 +48,11 @@ void print_buffer(char *b, int size)
 		printf("%08x: ", i * 10);
 		if (i < size / 10)
 		{
-
-print_line(b, 9, i);
+			print_line(b, 9, i);
 		}
 		else
 		{
-
-print_line(b, size % 10 - 1, i);
+			print_line(b, size % 10 - 1, i);
 		}
 		putchar('\n');
 	}
