334c161fce1e5003d3bdcde67a0cd676392bd948
Daud1000 <abdulazeezdaud726@gmail.com>
2022-12-23 03:17:45 -0800

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,8 +11,8 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-int i;
-for (i = 0; i < n && *(src + i); i++)
+	int i;
+        for (i = 0; i < n && *(src + i); i++)
 	{
 		*(dest + i) = *(src + i);
 	}
